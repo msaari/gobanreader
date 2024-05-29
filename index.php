f596799b0e57b1475b9a0fa2c7369f4536972ae7
Mikko Saari <mikko@mikkosaari.fi>
2024-05-29 08:27:54 +0300

    Allow commas and periods in komi

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		<h2>Pisteenlasku</h2>
 		<div class="row"><label for="white_captures">Vangitut valkoiset:</label> <input type="text" inputmode="numeric" pattern="[0-9]*" name="white_captures" value="0" /></div>
 		<div class="row"><label for="black_captures">Vangitut mustat:</label> <input type="text" inputmode="numeric" pattern="[0-9]*" name="black_captures" value="0" /></div>
-		<div class="row"><label for="komi">Komi:</label> <input type="text" inputmode="numeric" pattern="[0-9]*" name="komi" value="6.5" /></div>
+		<div class="row"><label for="komi">Komi:</label> <input type="text" inputmode="numeric" pattern="[0-9,\.]*" name="komi" value="6.5" /></div>
 
 		<input type="submit" value="Lähetä" />
 	</form>
